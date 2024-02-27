35f450cb878b015f37b30437d8133ed1e503d3e7
nhemmer730 <nhemmer730@gmail.com>
2024-02-27 11:25:44 -0500

    Add open source link on Github link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project is created by Nicole Hemmer is
+        <a href="https://github.com/nhemmer730/react-weather-app">
+          {" "}
+          open-sourced on GitHub.
+        </a>
+      </footer>
+    </div>
+  );
 }
