237c5bc52c795ec2a34171064c9c0cdcdd94467a
nhemmer730 <nhemmer730@gmail.com>
2024-02-27 14:11:42 -0500

    Added basic HTML structure to weather.js file

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
+import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project is created by Nicole Hemmer is
-        <a href="https://github.com/nhemmer730/react-weather-app">
-          {" "}
-          open-sourced on GitHub.
-        </a>
-      </footer>
+      <div className="container">
+        <Weather />
+        <footer>
+          This project is created by{" "}
+          <a href="https://willowy-nasturtium-57d59d.netlify.app">
+            Nicole Hemmer
+          </a>{" "}
+          is
+          <a href="https://github.com/nhemmer730/react-weather-app">
+            {" "}
+            open-sourced on GitHub.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
